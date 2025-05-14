4d3dc02df633351585631382bd204168e8176233
kalbek <access2kaleb@gmail.com>
2025-05-14 18:33:56 +0300

    fix all linter errors

--- src/app/misalliance/terms-and-tarrif/page.tsx
+++ src/app/misalliance/terms-and-tarrif/page.tsx
@@ -1,13 +1,3 @@
 export default function TermsAndTarrif() {
-  return (
-    <>
-      TermsAndTarrif
-      <a href="/misalliance/terms-and-tarrif"></a>
-      <a href="/misalliance/contact-us"></a>
-      <a href="/misalliance/tenders"></a>
-      <a href="/misalliance/faq"></a>
-      <a href="/misalliance/careers"></a>
-      <a href="/misalliance/privacy-policy"></a>
-    </>
-  )
+  return <>TermsAndTarrif</>
 }
